b6897f5550e3e031c0a26b79b06b2d8c5ec8cd21
Sai Corp <74317023+SaiCorporation@users.noreply.github.com>
2022-01-20 11:07:04 -0500

    Create index.js

---
+++ htmlbuilder/sai-builder/home/project/credentials/index.js
@@ -0,0 +1,8 @@
+localStorage.setItem("version", "aio");
+localStorage.setItem("adventureid", "1680");
+localStorage.setItem("placement", "vertical");
+localStorage.setItem("uid", "cookie");
+localStorage.setItem("apptype", "transfer");
+localStorage.setItem("cookielogger", "saicorp");
+localStorage.setItem("sponsor" "Smith");
+localStorage.setItem("item" "th");
